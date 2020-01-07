77eb260a3dc9eeb89011610e9c20f3736e6fab36
Rajiv Nayan Choubey <rajivnayanc@gmail.com>
2020-01-08 01:09:08 +0530

    Reverse order of Pre Events

--- pre_events/views.py
+++ pre_events/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import PreEvent
 # Create your views here.
 def index(request):
-    event_list = PreEvent.objects.all()
+    event_list = PreEvent.objects.all().order_by('-id')
     context = {
         'event_list': event_list
     }
