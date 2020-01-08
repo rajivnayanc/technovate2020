b81fbe91935aedc893ef6952f4cac078a3d855e6
Rajiv Nayan Choubey <rajivnayanc@gmail.com>
2020-01-09 03:00:49 +0530

    Blog App Complete and change brochure link

--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from .models import Blogs
 # Create your views here.
 def index(request):
-    return render(request,'blogs/index.html')
+    blogs = Blogs.objects.all().order_by('-post_date')
+    context = {
+        'blogs':blogs
+    }
+    return render(request,'blogs/index.html',context=context)
 
 def blogindex(request,id):
-    return render(request, 'blogs/posts.html')
+    blog = Blogs.objects.get(pk=id)
+    context = {
+        'blog':blog
+    }
+    return render(request, 'blogs/posts.html',context=context)
