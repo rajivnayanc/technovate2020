bd9ba618c5bff8f483cadd98186b9f6b3f6b5d11
Rajiv Nayan Choubey <rajivnayanc@gmail.com>
2020-02-21 05:52:41 +0530

    Added Register Button at HomePage and added model to store url and Link Text

--- HomePage/models.py
+++ HomePage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Links(models.Model):
+    url = models.URLField(blank=True)
+    price = models.CharField(max_length = 255,blank =True)
+
+    def __str__(self):
+        return self.url
+    
--- HomePage/views.py
+++ HomePage/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Links
 # Create your views here.
 def index(request):
-    return render(request,'HomePage/index.html')
+    output = Links.objects.get(pk=1)
+    return render(request,'HomePage/index.html',context={'output':output})
 
