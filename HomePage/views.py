1e779a8e6d4642c7b439e583d5adeb297f61681d
Rajiv Nayan Choubey <rajivnayanc@gmail.com>
2020-03-03 02:25:58 +0530

    Modified Home Page Book Link

--- HomePage/views.py
+++ HomePage/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from .models import Links
 # Create your views here.
 def index(request):
-    output = Links.objects.get(pk=1)
+    output = Links.objects.all()[:1].get()
     return render(request,'HomePage/index.html',context={'output':output})
 
